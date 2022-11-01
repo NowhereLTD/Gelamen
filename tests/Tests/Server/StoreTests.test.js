dcd2eb0136e303a6952ba5d6591a2550b3b9e805
Fr13dr1ch99 <fr13dr1ch99@gmail.com>
2022-11-01 18:38:30 +0100

    fix deno lint

--- tests/Tests/Server/StoreTests.test.js
+++ tests/Tests/Server/StoreTests.test.js
@@ -1,7 +1,5 @@
 import { SqlitePathQLDatabaseController } from "pathql/tests/DatabaseController/SqlitePathQLDatabaseController.class.js";
-import { Example } from "pathql/tests/Entrys/Example.pathql.js";
 import { User } from "pathql/tests/Entrys/User.pathql.js";
-import {assertEquals} from "https://deno.land/std@0.159.0/testing/asserts.ts";
 
 Deno.test("store test", async (_t) => {
 	const db = new SqlitePathQLDatabaseController({"name": "test.db"});
@@ -9,7 +7,7 @@ Deno.test("store test", async (_t) => {
 		const user = await new User({
 			"db": db
 		});
-		const storeRequest = await user.store({});
+		const _storeRequest = await user.store({});
 
 		db.close();
 	}catch(e) {
